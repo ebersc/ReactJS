405849312abfead653b1193b80bf87b20dbe1a2f
Eber <eberson.santoscosme@gmail.com>
2022-05-12 21:44:18 -0300

    Estilizando os cards

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css"
 import React from "react"
 
 import Card from "./components/layout/Card"
@@ -7,27 +8,27 @@ import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from "./Aleatorio"
 
 export default _ =>
-    <div id="app">
+    <div className="App">
         <h1>Fundamentos React</h1>
-        
-        <Card titulo="#04 - Desafio Aleatório">
-            <Aleatorio maior={100} menor={1}></Aleatorio>
-        </Card>
+        <div className="Cards">
+            <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
+                <Aleatorio maior={100} menor={1}></Aleatorio>
+            </Card>
 
-        <Card titulo="#03 - Fragmento">
-            <Fragmento></Fragmento>
-        </Card>
+            <Card titulo="#03 - Fragmento" color="#E94C6F">
+                <Fragmento></Fragmento>
+            </Card>
 
-        <Card titulo="#02 - Com parâmetro">
-            <ComParametro
-                titulo="Situação do aluno"
-                aluno="Pedro Silva"
-                nota={9.3} />
-        </Card>
+            <Card titulo="#02 - Com parâmetro" color="#E8b71A">
+                <ComParametro
+                    titulo="Situação do aluno"
+                    aluno="Pedro Silva"
+                    nota={9.3} />
+            </Card>
 
-        <Card titulo="#01 - Primeiro Componente">
-            <Primeiro></Primeiro>
-        </Card>
-
+            <Card titulo="#01 - Primeiro Componente" color="#588c73">
+                <Primeiro></Primeiro>
+            </Card>
+        </div>
     </div>
 
