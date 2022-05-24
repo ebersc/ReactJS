45ead999f3cc8389cef40b2536615da5a5a5c01e
Eber <eberson.santoscosme@gmail.com>
2022-05-24 19:57:11 -0300

    States e componentes controlados

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -14,11 +14,15 @@ import ParOuImpar from "./components/condicional/ParOuImpar"
 import UsuarioInfo from "./components/condicional/UsuarioInfo"
 import DiretaPai from "./components/comunicacao/DiretaPai"
 import IndiretaPai from "./components/comunicacao/IndiretaPai"
+import Input from "./components/formulario/Input"
 
 export default _ =>
     <div className="App">
         <h1>Fundamentos React</h1>
         <div className="Cards">
+            <Card titulo="#11 - Componente controlado" color="#168305">
+            <Input></Input>
+            </Card>
             <Card titulo="#10 - Comunicação indireta" color="#165005">
                 <IndiretaPai></IndiretaPai>
             </Card>
--- fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 export default props => {
     const cb = props.quandoClicar
+
+    const gerarIdade = () => parseInt(Math.random() * (20)) + 50;
     return(
         <div>
             <div>
                 Filho
             </div>
-            <button onClick={_ => cb('João', 53, true)}>
+            <button onClick={_ => cb('João', gerarIdade(), true)}>
                 Fornecer informações
             </button>
         </div>
